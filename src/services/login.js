569d82b22581739c3fdb448c4b78a6b5245c8971
Henry Rasinkangas <henry.rasinkangas@tuni.fi>
2022-03-16 19:27:22 +0200

    heroku version ready

--- src/services/login.js
+++ src/services/login.js
@@ -3,7 +3,6 @@ const baseUrl = `${process.env.REACT_APP_API_URL}/api/login`
 
 
 const login = async credentials => {
-  console.log(baseUrl)
   const response = await axios.post(baseUrl, credentials)
   return response.data
 }
