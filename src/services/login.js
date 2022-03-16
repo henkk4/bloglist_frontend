ee1d8413cea6c18c35f3d62f75795697a474fac2
Henry Rasinkangas <henry.rasinkangas@tuni.fi>
2022-03-16 19:21:47 +0200

    heroku config vars

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 const baseUrl = `${process.env.REACT_APP_API_URL}/api/login`
 
+
 const login = async credentials => {
+  console.log(baseUrl)
   const response = await axios.post(baseUrl, credentials)
   return response.data
 }
